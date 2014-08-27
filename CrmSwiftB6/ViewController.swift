8ef94f886407b9dabc2a507f48c6b4d7fa86aff6
chenjianAgain <joseph@josephtekiMacBook-Pro.local>
2014-08-27 15:15:22 +0800

    Initial Commit

---
+++ CrmSwiftB6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrmSwiftB6
+//
+//  Created by joseph on 8/27/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CrmSwiftB6Tests/CrmSwiftB6Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CrmSwiftB6Tests.swift
+//  CrmSwiftB6Tests
+//
+//  Created by joseph on 8/27/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CrmSwiftB6Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
